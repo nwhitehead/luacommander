efa253111ef380f2ceaaa748ef1a75421aba0c5b
Nathan Whitehead <nwhitehe@gmail.com>
2014-12-29 16:48:28 -0800

    Use docker to do Linux builds in a virtual Debian 6 container.

---
+++ docker/build.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+set -e
+
+# Get user configuration from file
+URL="https://github.com/nwhitehead/luacommander.git"
+BUILD_COMMAND="mkdir -p /root/build && cd /root/build && cmake /root/project && make && cpack"
+NAME="luacmd"
+GET_COMMAND="cd /root/build && tar cO *.zip | gzip -f"
+
+COMMAND=$1
+
+# Store files to indicate which tags are used in docker
+mkdir -p .tags
+
+# Make sure environment base image is setup
+docker build -t nwhitehead/build .
+
+if [ "$COMMAND" = "" ];
+then
+    if [ -e .tags/$NAME-latest ];
+    then
+        # File exists, that means we are in a ready state to do incremental build
+        # Run the build command
+        echo $BUILD_COMMAND
+        echo "You may need to remove the cid file if this fails"
+        docker run --cidfile="cid" -a stdout -a stdin $NAME/latest /bin/sh -c "$BUILD_COMMAND"
+        # Commit changes
+        docker commit `cat cid` $NAME/latest
+        rm cid
+        echo "BUILD SUCCEEDED"
+    else
+        # Checkout the code, create container
+        docker run --cidfile="cid" -a stdout -t nwhitehead/build git clone $URL /root/project
+
+        # Commit checkout state, set to be latest state
+        docker commit `cat cid` $NAME/checkout
+        touch .tags/$NAME-checkout
+        docker commit `cat cid` $NAME/latest
+        touch .tags/$NAME-latest
+        rm cid
+        echo "CHECKOUT SUCCEEDED"
+    fi
+else
+    echo "COMMAND " $COMMAND
+    if [ "$COMMAND" = "get" ];
+        echo "GETTTING"
+        docker run $NAME/latest /bin/sh -c "$GET_COMMAND" > result.tar.gz
+        echo "RESULTS ARE IN result.tar.gz"
+    then
+        echo "OTHER"
+    fi
+fi

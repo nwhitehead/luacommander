66c8bf5ac3fb2baf92751a8c39fc34e8e5de021d
Nathan Whitehead <nwhitehe@gmail.com>
2015-01-02 11:10:38 -0800

    Add fromCSV utility method.

--- src/boot.lua
+++ src/boot.lua
@@ -160,3 +160,33 @@ function re.fsplit(txt, delim, opt)
     end
     return res
 end
+
+-- Parse a line of CSV into an array of strings
+-- From:
+--     http://www.lua.org/pil/20.4.html
+function fromCSV(s)
+    s = s .. ','        -- ending comma
+    local t = {}        -- table to collect fields
+    local fieldstart = 1
+    repeat
+        -- next field is quoted? (start with `"'?)
+        if string.find(s, '^"', fieldstart) then
+            local a, c
+            local i  = fieldstart
+            repeat
+                -- find closing quote
+                a, i, c = string.find(s, '"("?)', i + 1)
+            until c ~= '"' -- quote not followed by quote?
+            if not i then error('unmatched "') end
+            local f = string.sub(s, fieldstart + 1, i - 1)
+            table.insert(t, (string.gsub(f, '""', '"')))
+            fieldstart = string.find(s, ',', i) + 1
+        else
+            -- unquoted; find next comma
+            local nexti = string.find(s, ',', fieldstart)
+            table.insert(t, string.sub(s, fieldstart, nexti - 1))
+            fieldstart = nexti + 1
+        end
+    until fieldstart > string.len(s)
+    return t
+end
